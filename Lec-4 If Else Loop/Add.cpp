e642891679834cb9b3111a1789950c3ef9889683
Atharv Pitrubhakta <74965270+AtharvPitrubhakta@users.noreply.github.com>
2023-09-29 20:40:25 +0530

    Add files via upload

---
+++ Lec-4 If Else Loop/Add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a , b;
+    cout << "Enter your first number:";
+    cin >> a;
+    cout << "Enter your second number:";
+    cin >> b;
+
+    cout << "The Sum number is:"<< a + b;
+}
---
+++ Lec-5 Advance Loop/For.cpp
@@ -0,0 +1,160 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+// To print 101 to 200 number.
+    // int i;
+
+    // for(i = 101; i <= 200; i++) {
+    //     cout << i << endl;
+    // }
+
+// To print a to z characters.
+
+    // for(char name = 'a';
+    // name <= 'z'; name++) {
+    //     cout << name << " ";
+    // }
+
+// To print reverse ex: 10 to 1 numbers.
+
+    // int n;
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // for(int i = n; i >= 1; i = i - 1) {
+    //     cout << i << " ";
+    // }
+
+// To print 1 - 100 number by 3 terms.
+
+    // int n;
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // for(int i = 1; i <= 100; i = i + 3){
+    //     cout << i << " ";
+    // }
+
+// To print the table program for any number.
+
+    // int n;
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // for(int i = 1; i <= 10; i++) {
+    //     cout << n << "*" << i << "=" << n * i << endl;
+    // }
+
+// WAP to print pow of any number;
+
+    // int n,pow,num;
+    // cout << "Enter your number: ";
+    // cin >> n;
+    // cout << "Enter your power: ";
+    // cin >> pow;
+
+    // num = n;
+    // for(int i = 1; i < pow; i++) {
+    //     num = num * n;
+    // }
+
+    // cout << num;
+     
+// WAP to sum of n natural numbers.
+
+    // int n;
+    // int sum = 0;
+
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // Pattern 1
+
+    // for(int i = 1; i <= n; i++) {
+    //     sum = sum + i;
+    // }
+
+    // cout << sum;
+
+
+    // Pattern 2
+
+    // cout << (n * (n+1) ) / 2;
+    
+
+// WAP to sum of first n(square) numbers.
+
+    // int n;
+    // int sum = 0;
+
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // for(int i = 1; i <= n; i++) {
+    //     sum = sum + i * i;
+    // }
+
+    // cout << sum;
+
+// WAP to print the number of Factorial.
+
+    // unsigned long long int n , fact = 1 , i;
+    // cout << "Enter your number: ";
+    // cin >> n;
+
+    // for(i = 1; i <= n; i++) {
+    //     fact = fact * i;
+    // }
+
+
+    // cout << fact;
+
+// WAP to check number is prime or not.
+
+    // int n;
+    // cout << "Enter the number: ";
+    // cin >> n;
+
+    // if(n < 2) {
+    //     cout << "Not a prime number";
+    //     return 0;
+    // }
+    // else{
+    //     for(int i = 2; i < n; i++) {
+    //         if(n % i == 0) {
+    //             cout << "Not a prime number";
+    //             return 0; 
+    //         }
+    //     }
+    //     cout << "This is a prime number";
+    //     return 0;
+    // }
+
+// WAP to print the Fibonacci Series.
+
+    // int n;
+    // cout << "Enter the number:";
+    // cin >> n;
+
+    // int last = 0, prev = 1,  curr = 1;
+
+    // if(n == 1) {
+    //     cout << last;
+    //     return 0;
+    // }
+
+    // for(int i = 3; i <= n; i++) {
+    //     curr = prev + last;
+    //     last = prev;
+    //     prev = curr;
+    // }
+
+    // cout << curr;
+
+
+
+    return 0;
+}
