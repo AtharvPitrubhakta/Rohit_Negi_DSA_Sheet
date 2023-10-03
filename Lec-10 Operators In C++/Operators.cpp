979b10d0b31d975db92aa8a1f257375546416a12
Atharv Pitrubhakta <74965270+AtharvPitrubhakta@users.noreply.github.com>
2023-10-03 14:53:50 +0530

    Add files via upload

---
+++ Lec-10 Operators In C++/Operators.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    // int a = 10;
+    // int b = a--;
+
+    // cout << b << " " << a; 
+
+    // comparison operator 
+    // 1 or 0
+
+    // int a, b;   
+    // cout << "Enter the value of a: ";
+    // cin >> a;
+    // cout << "Enter the value of b: "
+    // cin >> b;
+
+    // ==
+
+    // if(a == b) 
+    //     cout << "yes";
+    // else
+    //     cout << "no";
+    
+
+    // > , < , >= , <= , !=
+ 
+    // if(a != b) 
+    //     cout << "Yes";
+    // else 
+    //     cout << "No";
+
+    
+
+
+    // Logical Operators
+
+// And(&&) Operator
+    // int a, b, c;
+    // cin >> a >> b >> c;
+
+    // if(a> b && a > c) 
+    // cout << "Yes";
+    // else
+    // cout << "No";
+
+
+// Or(||) Operator
+
+    // char name = 'i';
+
+    // if(name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u')
+    // cout << "Vowel";
+    // else
+    // cout << "Consonant";
+
+// NOT(!) Operator
+
+    // cout << !23 <<endl;
+    // cout << !0 <<endl;
+
+
+
+// BITWISE OPERATORS
+
+    // // & operator
+    // int ans = 2 & 3;
+    // cout << ans << endl;
+
+    // // | operator
+    // int ans1 = 2 | 3;
+    // cout << ans1 << endl;
+
+    // // ^ operator
+    // int ans2 = 2 ^ 3;
+    // cout <<"My answer is: "<< ans2 << endl;
+
+    // // << operator
+    // int ans3 = 2 << 3;
+    // cout <<"My answer is: "<< ans3 << endl;
+
+    // // >>  operator
+    // int ans4 = 18 >> 3;
+    // cout <<"My answer is: "<< ans4 << endl;
+
+    // // ~  operator
+    // int ans5 = ~10;
+    // cout <<"My answer is: "<< ans5 << endl;
+
+
+
+// Assignment Operators
+
+    // int a = 10;
+    // a += 15; // a = a + 15;
+    // cout << a << endl;
+
+    // a -= 15; // a = a - 15;
+    // cout << a << endl;
+
+    if(8>5>3<2*4)
+    cout << "Yes";
+    else
+    cout << "No";
+
+
+
+
---
+++ Lec-10 Operators In C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+har name = 'i';
+
+    // if(name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u')
+    // cout << "Vowel";
+    // else
+    // cout << "Consonant";
